43aa8ada7ec1ed67e864441e0e462a360546d3f3
Octavius <octaviusbowman@gmail.com>
2021-09-07 18:03:39 -0400

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,43 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Octavius Bowman on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    @StateObject var locations = Locations()
+    
+    var body: some Scene {
+        WindowGroup {
+            TabView{
+                NavigationView {
+                    ContentView(location: locations.primary)
+                }
+                .tabItem {
+                    Image (systemName: "airplane.circle.fill")
+                    Text("Discover")
+                }
+                
+                NavigationView {
+                    WorldView()
+                }
+                .tabItem {
+                    Image (systemName: "star.fill")
+                    Text("Locations")
+                }
+                
+                NavigationView {
+                    TipsView()
+                }
+                .tabItem {
+                    Image (systemName: "list.bullet")
+                    Text ("Tips")
+                }
+            }.environmentObject(locations)
+        }
+    }
+}
